558e0273a774b3fe65c7e022a2cdf69f2f512ece
Ogunsola Isqeel <ogunsolaia@funaab.edu.ng>
2022-03-13 22:15:34 +0000

    I coppied the functions in my code to test- practical R file for testing

--- Tests/test-practical assignment codes.R
+++ Tests/test-practical assignment codes.R
@@ -5,3 +5,9 @@ source("./R_codes/practical assignment codes.R")
 
 testing_prob_y1()
 
+BOP2_design(N, lambda, gamma, n1, n2, theta)
+
+BOP2_design_improve(lambda, gamma, n1, n2, theta)
+
+sample_size_graph(lambda, gamma)
+
