0acf7db4ab17e967ffe0fff310fe8fda14ca6fd0
Ogunsola Isqeel <ogunsolaia@funaab.edu.ng>
2022-03-14 11:24:51 +0000

    The functions used in my BOP2 design were extracted so as to be able to run unit test for the required ones

--- Tests/test-practical assignment codes.R
+++ Tests/test-practical assignment codes.R
@@ -1,5 +1,5 @@
 
-testthat::test_dir("./Tests/test-practical assignmnet codes.R")
+#The functions used in my BOP2 design were extracted for so as to run unit test for the required ones
 
 source("./R_codes/practical assignment codes.R")
 
@@ -10,4 +10,3 @@ BOP2_design(N, lambda, gamma, n1, n2, theta)
 BOP2_design_improve(lambda, gamma, n1, n2, theta)
 
 sample_size_graph(lambda, gamma)
-
